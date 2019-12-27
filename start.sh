517b672bf7492a671be9b8a0b18566f260360933
Ryo Owada <64G806@gmail.com>
2019-12-27 11:53:43 +0900

    add osx-defaults

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+CURRENT=$(cd $(dirname $0);pwd)
+
+# install homebrew
+if ! which brew > /dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew doctor
+fi
+
+# install ansible
+if ! which ansible > /dev/null; then
+    brew install ansible
+fi
+
+ansible-playbook setup-mac.yml
